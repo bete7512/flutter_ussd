16df3e1c7e11e5a1667cb6daa9535eeb95ae7b33
bete7512 <betekbebe@gmail.com>
2022-10-08 21:00:05 +0300

    ask balance

--- ussd/lib/main.dart
+++ ussd/lib/main.dart
@@ -26,55 +26,55 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Ussd Plugin example'),
+          title: const Text('balance checking app'),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           children: [
-            TextField(
-              controller: _controller,
-              keyboardType: TextInputType.phone,
-              decoration: const InputDecoration(labelText: 'Ussd code'),
-            ),
-            if (_response != null)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Text(_response!),
-              ),
+            // TextField(
+            //   controller: _controller,
+            //   keyboardType: TextInputType.phone,
+            //   decoration: const InputDecoration(labelText: 'Ussd code'),
+            // ),
+            // if (_response != null)
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(vertical: 8),
+              //   child: Text(_response!),
+              // ),
             Row(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(
                   onPressed: () {
-                    UssdAdvanced.sendUssd(code: _controller.text, subscriptionId: 1);
+                    UssdAdvanced.sendUssd(code: '*804#', subscriptionId: 1);
                   },
-                  child: const Text('norma\nrequest'),
+                  child: const Text('check balance'),
                 ),
-                ElevatedButton(
-                  onPressed: () async {
-                    String? _res = await UssdAdvanced.sendAdvancedUssd(code: _controller.text, subscriptionId: 1);
-                    setState(() {
-                      _response = _res;
-                    });
-                  },
-                  child: const Text('single session\nrequest'),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    String? _res = await UssdAdvanced.multisessionUssd(code: _controller.text, subscriptionId: 1);
-                    setState(() {
-                      _response = _res;
-                    });
-                    String? _res2 = await UssdAdvanced.sendMessage('0');
-                    setState(() {
-                      _response = _res2;
-                    });
-                    await UssdAdvanced.cancelSession();
-                  },
-                  child: const Text('multi session\nrequest'),
-                ),
+                // ElevatedButton(
+                //   onPressed: () async {
+                //     String? _res = await UssdAdvanced.sendAdvancedUssd(code: _controller.text, subscriptionId: 1);
+                //     setState(() {
+                //       _response = _res;
+                //     });
+                //   },
+                //   child: const Text('single session\nrequest'),
+                // ),
+                // ElevatedButton(
+                //   onPressed: () async {
+                //     String? _res = await UssdAdvanced.multisessionUssd(code: _controller.text, subscriptionId: 1);
+                //     setState(() {
+                //       _response = _res;
+                //     });
+                //     String? _res2 = await UssdAdvanced.sendMessage('0');
+                //     setState(() {
+                //       _response = _res2;
+                //     });
+                //     await UssdAdvanced.cancelSession();
+                //   },
+                //   child: const Text('multi session\nrequest'),
+                // ),
               ],
             )
           ],
